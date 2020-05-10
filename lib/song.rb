2253e2a160ac58c19ab69e1adaf62415588f0bf2
Christopher Osborn <z68ozz@gmail.com>
2020-05-10 00:54:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,7 @@ class Song
   
   def self.genre_count
     genre_count = {}
-      @@genres.each {|genre| genre_count[genre] += 1}
+      @@genres.each {|genre| genre_count[genre] += 1} {|genre| genre_count[genre] += 1}
          genre_count
        end 
      
