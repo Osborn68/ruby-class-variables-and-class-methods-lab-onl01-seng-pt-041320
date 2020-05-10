f1c6a0549533e603ca7d2a27a69271a8369bed18
Christopher Osborn <z68ozz@gmail.com>
2020-05-10 00:38:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,7 @@ class Song
   @@genres = []
   @@artists = []
   
-  def intialize(name, artist, genre) 
+  def initialize(name, artist, genre) 
     @name = name
     @artist = artist
     @genre = genre
